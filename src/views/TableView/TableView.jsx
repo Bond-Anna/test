f813361265e0a2bfb4d2bd1a9c2d68a3b358db4e
Anna Bondarenko <anna93bondarenko@gmail.com>
2021-11-21 00:25:05 +0200

    add table

--- src/views/TableView/TableView.jsx
+++ src/views/TableView/TableView.jsx
@@ -3,10 +3,32 @@ import css from './tableView.module.css';
 
 const TableView = () => {
   const { data, error, isLoading } = useGetAllPeopleQuery();
-
+  // const people = ;
   return (
     <>
       <h1 className={css.text}>Table</h1>
+      {data && (
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Birth Year</td>
+              <td>Height</td>
+              <td>Mass</td>
+            </tr>
+          </thead>
+          <tbody>
+            {data.results.map(({ name, birth_year, height, mass }) => (
+              <tr key={name}>
+                <td>{name}</td>
+                <td>{birth_year}</td>
+                <td>{height}</td>
+                <td>{mass}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
